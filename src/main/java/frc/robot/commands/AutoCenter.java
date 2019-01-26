720f31a908c74f34387ca3bf948b0a471f9c9e3d
unknown <hunter.barclay18@gmail.com>
2019-01-25 17:06:26 -0800

    Did stuff

--- src/main/java/frc/robot/commands/AutoCenter.java
+++ src/main/java/frc/robot/commands/AutoCenter.java
@@ -13,22 +13,7 @@ public class AutoCenter extends CommandGroup {
    * Add your docs here.
    */
   public AutoCenter() {
-addSequential(new DriveToDistance(1));
-    // Add Commands here:
-    // e.g. addSequential(new Command1());
-    // addSequential(new Command2());
-    // these will run in order.
-
-    // To run multiple commands at the same time,
-    // use addParallel()
-    // e.g. addParallel(new Command1());
-    // addSequential(new Command2());
-    // Command1 and Command2 will run in parallel.
-
-    // A command group will require all of the subsystems that each member
-    // would require.
-    // e.g. if Command1 requires chassis, and Command2 requires arm,
-    // a CommandGroup containing them would require both the chassis and the
-    // arm.
+    addSequential(new DriveToDistance(10));
+    addSequential(new DriveToAngle(90));
   }
 }
