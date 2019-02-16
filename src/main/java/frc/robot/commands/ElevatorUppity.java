359536dd3e8d913e93b1ccf78e9782b165f6fa32
chsrobotics <hunter.barclay18@gmail.com>
2019-02-15 23:01:33 -0800

    The elevator and arm both work and are sketch

--- src/main/java/frc/robot/commands/ElevatorUppity.java
+++ src/main/java/frc/robot/commands/ElevatorUppity.java
@@ -27,7 +27,7 @@ public class ElevatorUppity extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    Robot.elevator.SetPower(.4);
+    Robot.elevator.SetPower(.3);
   }
 
   // Make this return true when this Command no longer needs to run execute()
