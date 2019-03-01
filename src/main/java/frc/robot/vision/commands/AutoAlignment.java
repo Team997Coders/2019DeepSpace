4860d1cdd72808204bd0c0e4325da19eabacd025
Rohan Pankaj <rohansp01@gmail.com>
2019-02-28 22:13:49 -0800

    Made RohanTest AUtoALigning. Need to change noLineSeen Boolean to static. Also need to map command to a button

--- src/main/java/frc/robot/vision/commands/AutoAlignment.java
+++ src/main/java/frc/robot/vision/commands/AutoAlignment.java
@@ -12,13 +12,15 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.subsystems.DriveTrain;
+import frc.robot.subsystems.LineDetector;
 
 
 
-public class RohanTest extends Command {
+public class AutoAlignment extends Command {
 
-  public RohanTest() {
-    requires DriveTrain
+  public AutoAlignment() {
+    requires(Robot.driveTrain);
+    requires(Robot.backLineDetector);
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
   }
@@ -31,7 +33,24 @@ public class RohanTest extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    
+  
+   double cameraAngle = Robot.logitechVisionOI.getVisionLeftXAxis();
+
+    while(cameraAngle >= 90 ){
+      Robot.driveTrain.setVolts(-.5,-.5);//Negative because the camera is on the back
+      while(cameraAngle !=90){
+        Robot.driveTrain.setVolts(.5,-.5);
+      }
+    }
+    while(cameraAngle <= 90){
+      Robot.driveTrain.setVolts(-.5, -.5);//Negative because the camera is on the back
+      while(cameraAngle !=90){
+        Robot.driveTrain.setVolts(-.5,.5);
+      }
+    }
+    if(cameraAngle == 90 && LineDetector.noLineSeen()){ //TODO: make 'noLineSeen' static before testing
+      Robot.driveTrain.setVolts(-.5, -.5); //Negativve because the camera is on the back
+    }    
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -51,3 +70,29 @@ public class RohanTest extends Command {
   protected void interrupted() {
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
