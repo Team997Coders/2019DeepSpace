4cebddbeea64eab704b390606f5a092d24068407
Floyd Moore <floyd.moore@gmail.com>
2019-04-29 00:07:24 -0700

    compiles clean

--- src/main/java/frc/robot/vision/ProcessVision.java
+++ src/main/java/frc/robot/vision/ProcessVision.java
@@ -7,25 +7,34 @@
 
 package frc.robot.vision;
 
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.vision.VisionThread;
 import frc.robot.vision.GripPipeline;
 
 /**
  * Add your docs here.
  */
 public class ProcessVision {
-    double centerX;
-    UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
-    
-    visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
-        if (!pipeline.filterContoursOutput().isEmpty()) {
-            Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
-            synchronized (imgLock) {
-                centerX = r.x + (r.width / 2);
-            }
-        }
-    });
-    visionThread.start();
+    private static final int IMG_WIDTH = 320;
+    private static final int IMG_HEIGHT = 240;
+    private VisionThread visionThread;
+    private double centerX = 0.0;
+    private final Object imgLock = new Object();
+
+    public ProcessVision() {
+        UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+        camera.setResolution(IMG_WIDTH, IMG_HEIGHT);
   
+        visionThread = new VisionThread(camera, new GripPipeline(), pipeline -> {
+            if (!pipeline.filterContoursOutput().isEmpty()) {
+                Rect r = Imgproc.boundingRect(pipeline.filterContoursOutput().get(0));
+                synchronized (imgLock) {
+                    centerX = r.x + (r.width / 2);
+                }
+            }
+        });
+    }
 }
